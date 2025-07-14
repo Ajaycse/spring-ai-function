529df49b9eeb8e806e3f06de015726122a606c66
Ajay Kumar Manjhi <ajay.cse.nitjsr@gmail.com>
2025-07-14 15:38:15 +0530

    added logger in the Weather service

--- src/main/java/com/xapp/ai/function/service/WeatherService.java
+++ src/main/java/com/xapp/ai/function/service/WeatherService.java
@@ -5,14 +5,19 @@ import com.xapp.ai.function.service.WeatherService.Request;
 import com.xapp.ai.function.service.WeatherService.Response;
 import java.util.List;
 import java.util.function.Function;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestClient;
 
 @Component
+@Slf4j
 public class WeatherService implements Function<Request, Response> {
 
+  private static final Logger log = LoggerFactory.getLogger(WeatherService.class);
   private final WeatherConfigProperties weatherConfigProperties;
   private final RestClient restClient;
 
@@ -45,11 +50,14 @@ public class WeatherService implements Function<Request, Response> {
     map.add("x-rapidapi-key", weatherConfigProperties.key());
     map.add("x-rapidapi-host", weatherConfigProperties.host());
 
+    log.debug("rest call to weather service started");
+
     Response response = restClient.get()
         .uri("/{city}/EN", request.location())
         .headers(headers -> headers.addAll(map))
         .retrieve()
         .body(Response.class);
+    log.debug("rest call to weather service ended");
     return response;
   }
 
